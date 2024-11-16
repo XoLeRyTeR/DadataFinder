e3f480de46a61b7b63d285997a3c6cc78bc3059a
Андрей <lisa1980@inbox.ru>
2024-11-16 21:22:29 +0300

    test

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 from dadata import Dadata
-from datetime import datetime
+import datetime
 import sqlite3
 import json
 
@@ -20,9 +19,11 @@ if "page" not in st.session_state:
     st.session_state.page = "Главная"
 if "result" not in st.session_state:
     st.session_state.result = None
+if "button_clicked" not in st.session_state:
+    st.session_state.button_clicked = False
 def unix_to_date(x):
     try:
-        dt = datetime.utcfromtimestamp(x / 1000)
+        dt = datetime.datetime.utcfromtimestamp(x / 1000)
         str=dt.strftime('%Y-%m-%d')
         return str
     except:
@@ -34,7 +35,7 @@ def prepross():
     df["data.state.registration_date"] = df["data.state.registration_date"].apply(unix_to_date)
     df["data.state.liquidation_date"] = df["data.state.liquidation_date"].apply(unix_to_date)
     df["data.ogrn_date"] = df["data.ogrn_date"].apply(unix_to_date)
-
+    df['data.management.start_date'] = df['data.management.start_date'].apply(unix_to_date)
     df = df.reindex(columns=data_translate.keys())
     df = df.rename(columns=data_translate)
     return df
@@ -62,6 +63,7 @@ def search_inn(inn):
         st.warning("Организация с таким ИНН не найдена.")
 
 def come_back():
+    st.session_state.button_clicked = False
     st.session_state.page = "Главная"
     st.session_state.result = None
 def come_find():
@@ -70,18 +72,23 @@ def come_find():
 def come_BD():
     st.session_state.page = 'База данных'
 
+def come_tabl():
+    st.session_state.page= 'питоняшка'
+
 if st.session_state.page=="База данных":
     st.title("База данных компаний")
     all_users = pd.read_sql("SELECT * FROM users", conn)
-    st.dataframe(all_users, width=10000)
+    st.dataframe(all_users, width=1000000000)
     st.button('Поиск по ИНН', on_click=come_find)
 
 elif st.session_state.page == "Главная":
-    col1,col2 = st.columns([4,1])
+    col1,col2,col3 = st.columns([4,1.2,1])
     with(col1):
         st.subheader('Введите ИНН')
     with col2:
         st.button('Все компании',on_click=come_BD)
+    with col3:
+        st.button('Таблица',on_click=come_tabl)
     with st.form(key="inn_form"):
         inn = st.text_input("Введите ИНН")
         submit_button = st.form_submit_button("Найти")
@@ -94,7 +101,58 @@ elif st.session_state.page == "Информация по ИНН":
 
     df = prepross().head(1)
     st.write(df)
-
     sql_query(df)
+elif st.session_state.page == "питоняшка":
+    st.button('Вернуться',on_click=come_back)
+    table = pd.read_sql("SELECT * FROM 'table'", conn)
+    st.dataframe(table)
+    if not st.session_state.button_clicked:
+        if st.button("Добавить новую запись"):
+            st.session_state.button_clicked = True
+            st.rerun()
+    if st.session_state.button_clicked:
+        mp={}
+        with st.form(key='form'):
+            mp['Номер на ЕФРСБ']=st.text_input('Введите номер на ЕФРСБ')
+            mp['Наименование должника']=st.text_input('Введите наименование должника')
+            mp['Вид торгов'] = st.text_input('Введите вид торгов')
+            mp['Дата начала подачи заявок'] = st.date_input('Введите дату начала подачи заявок',min_value=datetime.date(2000, 1, 1), max_value=datetime.date(2030, 12, 31))
+            mp['Дата окончания подачи заявок'] = st.date_input('Введите дату окончания подачи заявок',min_value=datetime.date(2000, 1, 1), max_value=datetime.date(2030, 12, 31))
+            mp['Место проведения'] = st.text_input('Введите место проведения')
+            mp['Номер лота'] = st.text_input('Введите номер лота')
+            mp['Начальная цена'] = st.text_input('Введите начальную цену')
+            mp['Информация о снижении'] = st.text_input('Введите информацию о снижении')
+            mp['Описание'] = st.text_input('Введите описание')
+            mp['ИНН'] = st.text_input('Введите ИНН')
+            mp['Номер арбитражного дела'] = st.text_input('Введите номер арбитражного дела')
+            mp['(3) Дата отчетного года'] = st.date_input('Введите дату отчетного года',min_value=datetime.date(2000, 1, 1), max_value=datetime.date(2030, 12, 31))
+            mp['Сумма ДЗ'] = st.text_input('Введите cумму ДЗ')
+            mp['(3) Сумма общих долгов'] = st.text_input('Введите сумму общих долгов')
+            mp['(3) Процентное отношение ДЗ к общим долгам общества'] = st.text_input('Введите процентное отношение ДЗ к общим долгам общества')
+            mp['(2) Статус'] = st.radio('Введите статус',('Действующее','Не действующее'))
+            mp['(2) Наличие недостоверных сведений'] = st.radio('Есть ли наличие недостоверных сведений',('Да','Нет'))
+            mp['(2) Заинтересованность третьих лиц'] = st.radio('Есть ли заинтересованность третьих лиц',('Да','Нет'))
+            mp['(2) Наличие блокировок от налоговой'] = st.radio('Есть ли наличие блокировок от налоговой',('Да','Нет'));
+            mp['(2) Наличие гос участия'] = st.radio('Наличие гос участия',('Без гос участия','С гос участием','Не определено'))
+            mp['(2) Наличие у руководителя должника сторонних действующих обществ'] = st.text_input('Наличие у руководителя должника сторонних действующих обществ (если есть указывается ИНН, наименование и чистые активы общества в одном поле)')
+            mp['(2) Наличие у участника должника сторонних действующих обществ'] = st.text_input('Наличие у участника должника сторонних действующих обществ (если есть указывается ИНН, наименование и чистые активы общества в одном поле)')
+            mp['(2) Смена руководителя'] = st.date_input('Введите дату смены руководителя',min_value=datetime.date(2000, 1, 1), max_value=datetime.date(2030, 12, 31))
+            mp['(2) Смена участника'] = st.date_input('Введите дату смены участника',min_value=datetime.date(2000, 1, 1), max_value=datetime.date(2030, 12, 31))
+            mp['(2) Количество юр лиц директора'] = st.text_input('Введите количество юр лиц директора')
+            mp['(2) Количество юр лиц участника'] = st.text_input('Введите количество юр лиц участника')
+            mp['(3) Сумма основных средств (ОС)'] = st.text_input('Введите сумму основных средств (ОС)')
+            mp['(3) Динамика изменения ОС'] = st.text_input('Введите динамику изменения ОС')
+            mp['(3) Сумма запасов'] = st.text_input('Введите сумму запасов')
+            mp['(3) Динамика изменения запасов'] = st.text_input('Введите динамику изменения запасов')
+            mp['(3) Чистые активы'] = st.text_input('Введите чистые активы')
+            mp['Наличие задолженностей (для физ лиц)'] = st.text_input('Введите количество и общая сумма непогашенной задолженности (ФССП)')
+            mp['Итоговая оценка'] = st.text_input('Введите итоговую оценку')
+            mp['Судебная практика'] = st.text_input('Введите судебную практику')
+            send = st.form_submit_button('Добавить запись')
+        if (send):
+            df = pd.DataFrame([mp])
+            df.to_sql('table', conn, if_exists='append', index=False)
+            st.success("Строка добавлена!")
+            st.session_state.button_clicked = False
+            st.rerun()
 
-    st.json(st.session_state.result)
