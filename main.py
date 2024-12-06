80ef796467ee7ad18d1e4401969886a31fafe18e
Андрей <lisa1980@inbox.ru>
2024-12-06 16:18:32 +0300

    add authorization test 8

--- main.py
+++ main.py
@@ -175,7 +175,7 @@ elif st.session_state.page == "Таблица с заполнением":
 elif st.session_state.page == 'Авторизация':
     col1,col2,col3 = st.columns([1,1,1])
     with col2:
-        st.subheader('Авторизация')
+        st.subheader('Авторизацияяяяя')
         login = st.text_input('Логин')
         password = st.text_input('Пароль')
         if st.button('Войти'):
